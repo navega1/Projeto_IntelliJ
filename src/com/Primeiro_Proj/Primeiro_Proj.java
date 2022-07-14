76eae53109e6e2cef2926dae1ba631d5f8b9c35f
Bruno Navega <navega.bruno@gmail.com>
2022-07-14 13:10:00 -0300

    Atualização

---
+++ src/com/Primeiro_Proj/Primeiro_Proj.java
@@ -0,0 +1,16 @@
+package com.Primeiro_Proj;
+
+import com.Primeiro_Proj.Model.Gato;
+
+public class Primeiro_Proj {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 2;
+        int b = 20;
+
+        System.out.println("Teste inicial " + (a+b));*/
+    }
+}
